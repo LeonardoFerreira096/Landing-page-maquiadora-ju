15b81213ab001750e49c88bc11cbf2d3cdf76c1f
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-04-15 22:55:21 -0300

    iniciando LP: primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>               
+      <nav>                
+        <a>Início</a>
+        <a>Galeria</a>
+        <a>Blog</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
